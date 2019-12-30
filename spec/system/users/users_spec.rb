812fa66d1328f0ce9835a7f10ffcb1ab846e7a99
DaichiSaito <hrkedz@gmail.com>
2019-12-30 11:30:15 +0900

    userのscaffoldとスペックを書いた

---
+++ spec/system/users/users_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :system do
+  before do
+    driven_by :selenium, using: :headless_chrome
+  end
+
+  it 'is valid' do
+    visit new_user_path
+    fill_in 'Name', with: 'yamada'
+    fill_in 'Age', with: 30
+    click_button 'Create User'
+    expect(page).to have_content 'User was successfully created.'
+  end
+
+  it 'is invalid without name' do
+    visit new_user_path
+    fill_in 'Age', with: 30
+    click_button 'Create User'
+    expect(page).to have_content "Name can't be blank"
+  end
+end
